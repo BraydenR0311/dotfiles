0b56a0b411661e59e24c74f1531fca3ed5774d6c
brayden <brayden.riesberg@aol.com>
2025-06-15 14:04:10 -0500

    add lsp config stuff and sql

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -819,6 +819,7 @@ require("lazy").setup({
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
 				python = { "ruff_format" },
+				sql = { "sql_formatter" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				javascript = { "prettierd", "prettier", stop_after_first = true },
