315830ef72dc396a2967fcb7dd669f0736a4b00a
brayden <brayden.j.riesberg@erdc.dren.mil>
2025-09-22 14:17:19 -0500

    add everforest

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -959,7 +959,18 @@ require("lazy").setup({
 		end,
 	},
 	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
-
+	{
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function()
+			require("everforest").setup({
+				-- Your config here
+			})
+		end,
+	},
 	-- Highlight todo, notes, etc in comments
 	{
 		"folke/todo-comments.nvim",
