ef0943a84b6e08ca9e2a2f7a2a26f7d58b79fb31
brayden <brayden.j.riesberg@erdc.dren.mil>
2026-05-23 20:25:38 -0500

    configure blink

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -862,10 +862,12 @@ do
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      ghost_text = { enabled = true },
+      list = { selection = { auto_insert = false } },
     },
 
     sources = {
-      default = { 'lsp', 'path', 'snippets' },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
 
     snippets = { preset = 'luasnip' },
