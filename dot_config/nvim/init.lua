d1b06aabb185d9b4b82e8e64c632486a9def3f0a
brayden <brayden.j.riesberg@erdc.dren.mil>
2026-06-29 12:37:23 -0500

    add toggleterm

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -233,7 +233,8 @@ do
   vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
   vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
   vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
+  
+  vim.keymap.set('n', '<leader>tt', ':ToggleTerm<cr>', { desc = '[T]oggle [Terminal]' })
   -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
   -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
   -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -895,6 +896,11 @@ do
     -- Shows a signature help window while you type arguments for a function
     signature = { enabled = true },
   }
+end
+
+do
+  vim.pack.add { { src = gh 'akinsho/toggleterm.nvim' } }
+  require('toggleterm').setup()
 end
 
 -- ============================================================
