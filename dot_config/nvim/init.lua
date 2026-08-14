207980e1928cb8d78959eb4bcad3f5c008aafff7
brayden <brayden.j.riesberg@erdc.dren.mil>
2026-08-14 11:59:44 -0500

    add windows clip support

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -992,4 +992,15 @@ do
   -- require 'custom.plugins'
 end
 
--- The line beneath this is called `modeline`. See `:help modeline`
+vim.g.clipboard = {
+     name =  'WslClipboard',
+     copy =  {
+       ["+"]  = 'clip.exe',
+       ["*"] = 'clip.exe',
+     },
+     paste =  {
+       ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+       ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+     },
+     cache_enabled = 0,
+   }
